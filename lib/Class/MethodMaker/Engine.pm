839424f4b70fb2791914ee1ccf4b19be5d167891
Steffen Schwigon <steffen.schwigon@amd.com>
2011-03-02 21:26:02 +0100

    version++

--- lib/Class/MethodMaker/Engine.pm
+++ lib/Class/MethodMaker/Engine.pm
@@ -57,7 +57,7 @@ BEGIN {
 # -------------------------------------
 
 our $PACKAGE = 'Class-MethodMaker';
-our $VERSION = '2.16';
+our $VERSION = '2.17';
 
 # -------------------------------------
 # CLASS CONSTRUCTION

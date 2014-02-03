6f958e5e5dbef1e0547b63d58396f1d8457aec75
Steffen Schwigon <ss5@renormalist.net>
2014-02-03 22:19:31 +0100

    version++

--- lib/Class/MethodMaker/Engine.pm
+++ lib/Class/MethodMaker/Engine.pm
@@ -57,7 +57,7 @@ BEGIN {
 # -------------------------------------
 
 our $PACKAGE = 'Class-MethodMaker';
-our $VERSION = '2.19';
+our $VERSION = '2.20';
 
 # -------------------------------------
 # CLASS CONSTRUCTION
